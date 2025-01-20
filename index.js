f54984d3341659fa5ac07504af7e2b7c042610d6
Alibro005 <itsalibro8@gmail.com>
2025-01-20 18:58:44 +0500

    update the wensite

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.getElementById("navbar");
+
+hamburger.addEventListener("click", () => {
+    navbar.classList.toggle("show")
+})
